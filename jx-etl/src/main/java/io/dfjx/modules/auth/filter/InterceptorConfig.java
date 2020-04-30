41fd56f8912b67878443b0ebf11d3c523a2a5dbc
cfchen01 <674441755@qq.com>
2020-04-30 10:36:53 +0800

    拦截器修改影响日期格式化

--- jx-etl/src/main/java/io/dfjx/modules/auth/filter/InterceptorConfig.java
+++ jx-etl/src/main/java/io/dfjx/modules/auth/filter/InterceptorConfig.java
@@ -3,22 +3,24 @@ package io.dfjx.modules.auth.filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class InterceptorConfig extends WebMvcConfigurationSupport {
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    /**
+     * 注册拦截器
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         LoginInterceptor loginInterceptor = new LoginInterceptor();
         registry.addInterceptor(loginInterceptor).addPathPatterns("/**")
                 .excludePathPatterns("/loginback")
                 .excludePathPatterns("/logincas");
-        super.addInterceptors(registry);
     }
 
     @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
-        super.addResourceHandlers(registry);
     }
 }
